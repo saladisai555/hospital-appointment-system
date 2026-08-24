d3a15894512a8f4093132be45d08bfd82a808081
Sai <saladisai555@gmail.com>
2026-08-24 15:27:57 +0530

    updated securityconfig

--- src/main/java/com/example/hospital_appointment_system/config/SecurityConfig.java
+++ src/main/java/com/example/hospital_appointment_system/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.hospital_appointment_system.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -19,5 +21,11 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
